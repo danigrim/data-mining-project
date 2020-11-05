9debe9a7f8b75783265fa14bd41ce12bfe33eeed
danigrim <danigrim@stanford.edu>
2020-11-05 14:39:14 +0200

    find-hrefs

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-URL='https://techcrunch.com/extracrunch/market-analysis/'
+URL='https://techcrunch.com/'
 
 class Scraper:
     def __init__(self, url):
@@ -13,7 +14,33 @@ class Scraper:
 
     def scrape(self):
         soup = BeautifulSoup(self.page.content, 'html.parser')
-        print(soup.prettify())
+        # divs = soup.find_all(class_='mini-view')
+        # print(divs)
+        # for div in divs:
+        #     articles = div.findChildren("article", recursive=True)
+        #     for a in articles:
+        #         h3 = a.findChildren("h3", recursive=True)
+        #         for h in h3:
+        #             links = h.findChildren("a", recursive=True)
+        #             for link in links:
+        #                 link['href']
+
+        article_titles = soup.find_all(href=True, class_="post-block__title__link")
+        # get all titles
+        articles = set()
+        for title in article_titles:
+           articles.add(title['href'])
+        print(articles)
+
+
+
+        #article_links = article_divs.find(href = True)
+
+       # print()
+        #links = soup.find_all('article', )
+       # print(links)
+        #pages = [a.find('a')['href'] for a in links if a != '']
+        #print(pages)
 
 
 if __name__ == '__main__':
