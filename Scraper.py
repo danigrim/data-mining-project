90b96a4774c9eebe156d6f601ebb784e93781b4a
danigrim <danigrim@stanford.edu>
2020-11-05 20:11:57 +0200

    feat/load_more_selenium

--- Scraper.py
+++ Scraper.py
@@ -1,47 +1,52 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 
 URL='http://techcrunch.com'
-ARTICLE = 'https://techcrunch.com/2020/11/05/alibaba-passes-ibm-in-cloud-infrastructure-market-with-over-2b-in-revenue/'
 
 class Scraper:
     def __init__(self, url):
-        self.page = requests.get(url)
-        if self.page.status_code == requests.codes.ok:
-            print("URL Fetching successful...\n")
-        else:
-            raise Exception("Issue with get request to url")
+        self.url = url
+
 
     def scrape(self):
         driver = webdriver.Chrome('./chromedriver')
-        driver.get(URL)
-        main_pg = driver.page_source
-        soup_wd = BeautifulSoup(main_pg, 'html.parser')
-        h3s = soup_wd.find_all("h3")
-        print(len(h3s))
-
-        soup = BeautifulSoup(self.page.content, 'html.parser')
-        article_titles = soup.find_all(href=True, class_="post-block__title__link")
-        # # get all titles
+        driver.get(self.url)
         articles = set()
-        for a in article_titles:
-           articles.add(a['href'])
 
-        self.get_article_info(articles)
+        while True:
+            curr_articles = set() #this will get current articles to avoid duplicating
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            featured_articles = soup.find_all("h3", class_='mini-view__item__title')
+            for a in featured_articles:
+                articles.add(a.findChildren('a')[0]['href'])
+            latest_articles = soup.find_all(href=True, class_="post-block__title__link")
+            # # get all from latest articles
+            for a in latest_articles:
+                articles.add(a['href'])
+
+            LOAD_MORE_BUTTON_XPATH = '//*[@id="tc-main-content"]/div[2]/div/div/button/span'
+            loadMoreButton = driver.find_element_by_xpath(LOAD_MORE_BUTTON_XPATH)
+
+            action = ActionChains(driver)
+            action.move_to_element(loadMoreButton).click().perform()
+            time.sleep(3)
+            self.get_article_info(articles)
 
 
     def get_article_info(self, articles):
-        driver = webdriver.Chrome('./chromedriver')
-        driver.get(ARTICLE)
-        menu_items = driver.find_elements_by_class_name('menu__item')
-        print(len(menu_items))
-        article = driver.page_source
-        soup_2 = BeautifulSoup(article, 'html.parser')
-        lis = soup_2.find_all('h3')
-        print(len(lis))
-        driver.close()
+        print(len(articles))
+        # driver = webdriver.Chrome('./chromedriver')
+        # driver.get(ARTICLE)
+        # menu_items = driver.find_elements_by_class_name('menu__item')
+        # print(len(menu_items))
+        # article = driver.page_source
+        # soup_2 = BeautifulSoup(article, 'html.parser')
+        # lis = soup_2.find_all('h3')
+        # print(len(lis))
+        # driver.close()
 
 
             
